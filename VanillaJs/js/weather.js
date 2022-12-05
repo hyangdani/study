87a87adba30b4a49bcaa133318e41c521813bdd4
hyangdani <nhyangs@gmail.com>
2022-12-05 17:20:29 +0900

    Create weather.js

---
+++ VanillaJs/js/weather.js
@@ -0,0 +1,26 @@
+const API_KEY = "de6181447712e6ac6401751c94a6a66b";
+
+function onGeoOk(position){
+    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    console.log("you live in", lat, lon);
+
+    fetch(url).then(response => response.json().then(data =>{
+        //const weatherContainer = document.getElementById("weather");
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+
+    }));
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
