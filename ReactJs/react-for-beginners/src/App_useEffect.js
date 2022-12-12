55d032a94d2436e8f599fc619c0684ff18e64526
hyangdani <nhyangs@gmail.com>
2022-12-12 14:03:44 +0900

    Update App_useEffect.js
    advanced

--- ReactJs/react-for-beginners/src/App_useEffect.js
+++ ReactJs/react-for-beginners/src/App_useEffect.js
@@ -1,6 +1,14 @@
-//import Button from "./Button";
-//import styles from "./App.module.css";
 import { useState, useEffect } from "react";
+
+function Hello(){
+  useEffect(() => {
+    console.log("hi :-)");
+    return function(){
+      console.log("bye :-)");
+    }
+  },[]);
+  return <h1>Hello</h1>;
+}
 
 function App() {
   const [counter, setValue] = useState(0);
@@ -28,17 +36,17 @@ function App() {
     console.log("I run when 'keyword&counter' changes.");
   }, [keyword, counter]);
 
+   
+  const [showing, setShowing] = useState(true);
+  const onClickShowing = () => setShowing((prev) => !prev);
+
   return (
-    /* create react app
-    <div>
-      <h1 className={styles.title}>Welcome back!</h1>    
-      <Button text={"Continue"}/>
-    </div>
-    */
     <div>
       <input onChange={onChange} type="text" placeholder="Search here.." value={keyword}/>
       <h1>{counter}</h1>    
-      <button onClick={onClick}>click me</button>
+      <button onClick={onClick}>click me</button><br/>
+      {showing?<Hello/>:null}
+      <button onClick={onClickShowing}>{showing?"Hide":"Show"}</button>
     </div>
   );
 }
